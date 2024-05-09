fe4c46acc618fc76f1b35bf88174c2beaaccb3ae
manulecam <lecam.manuel@gmail.com>
2024-05-09 13:11:35 -0300

    Arreglo nav seleccion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    link.className = "seleccionado";
+
+    var x = document.getElementById("nav");
+    x.className = "";
+
+}
+
 function responsiveMenu() {
     var x=document.getElementById("nav");
     if(x.className===""){
